d0b3fdac4491cbc82053a748b79e4638cd43de55
Kirill <stolpnikk.97@gmail.com>
2020-12-09 01:26:25 +0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create!(name: 'Работа')
+Project.create!(title: 'Сдать проект', category_id: '1')
+Project.create!(title: 'Научиться Angular\'у', category_id: '1')
+Project.create!(title: 'Выполнить работу', category_id: '1')
