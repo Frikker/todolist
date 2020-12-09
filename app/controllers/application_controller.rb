fa1b8f063a2f8da816a9916da34076404a2cd878
Frikker <stolpnikk.97@gmail.com>
2020-12-09 03:37:27 +0300

    fixed post and patch methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+
+  def validate(outcome)
+    if outcome.valid?
+      render json: { message: 'Success' }, status: :created
+    else
+      render json: outcome.errors, status: :unprocessable_entity
+    end
+  end
 end
